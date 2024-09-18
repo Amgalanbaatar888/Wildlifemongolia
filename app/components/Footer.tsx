0bd75b006606b1704914f68ced11034839b6ec49
Amgalanbaatar <amgalanbaatar@Amgalanbaatars-MacBook-Pro.local>
2024-09-18 20:28:18 +0800

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        &copy; 2021 Tumeco Wildlife
+      </div>
+    </div>
+  );
+}
